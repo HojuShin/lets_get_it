867608b71654e479c94dd3f762bcefbd7379bad2
HojuShin <shin03188@naver.com>
2022-06-07 18:20:35 +0900

    Reduce()

--- 반응속도테스트/QUIZ.js
+++ 반응속도테스트/QUIZ.js
@@ -4,3 +4,36 @@
 //(밀리초) / 1000 / 60 : 분
 //(밀리초) / 1000 / 60 / 60: 시간
 //(밀리초) / 1000 / 60 / 60 / 24 : 일
+
+//reduce 함수
+let userList = [
+    { name: "mike", age: 30 },
+    { name: "tom", age: 10 },
+    { name: "jane", age: 27 },
+    { name: "sue", age: 26 },
+    { name: "harry", age: 42 },
+    { name: "steve", age: 60 },
+]
+
+let result = userList.reduce((prev, cur) => {
+    if (cur.age > 19) {
+        prev.push(cur.name);
+    }
+    return prev;
+}, []) //초기값->빈배열에 넣어 반환
+
+//총 나이 구하기
+let result = userList.reduce((prev, cur) => {
+    return prev + cur.age;
+}, 0)
+
+//이름이 3글자
+let result = userList.reduce((prev, cur) => {
+    if (cur.name.length === 3) {
+        prev.push(cur.name);
+    }
+    return prev;
+}, [])
+console.log(result);
+
+//
