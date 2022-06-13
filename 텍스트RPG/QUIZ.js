bf31b49a1e0e7a6f9c4229cd8daf13439162baaa
HojuShin <shin03188@naver.com>
2022-06-14 01:45:56 +0900

    Class_extends

--- 텍스트RPG/QUIZ.js
+++ 텍스트RPG/QUIZ.js
@@ -1,7 +1,6 @@
 //Q.클래스-사람, 생성자 메서드-이름,나이를 속성으로 입력받으시오.
 //자신의 이름과 나이를 콘솔에 출력하는 메서드도 두개 만드시오
-
-class Person {
+class Person { //부모
     constructor(name, age) {
         this.name = name;
         this.age = age;
@@ -14,3 +13,11 @@ class Person {
     }
 }
 
+//Q. HTML, CSS, JS를 할 줄 아는 개발자를 만들어 봅시다.
+class Developer extends Person {
+    constructor(name, age, language) {
+        super(name, age);
+        this.language = language;
+    }
+}
+const developer = new Developer('shin', 27, ['HTML','CSS','js']);
